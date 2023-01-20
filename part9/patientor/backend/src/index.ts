3ca671b36fd033d02f46f49af1b626af4d9c3d35
JacobGuiang <jacob.guiang1@gmail.com>
2023-01-19 18:58:24 -0600

    Exercises 9.10 - 9.11

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
+import diagnoseRouter from './routes/diagnose';
+import patientRouter from './routes/patient';
 import cors from 'cors';
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3001;
+app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients', patientRouter);
 
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
