a37a7452cef26b230c736fc7bc6972c01b4e8537
JacobGuiang <jacob.guiang1@gmail.com>
2023-01-19 22:27:32 -0600

    Exercises 9.12 - 9.13

--- part9/patientor/backend/src/services/patientService.ts
+++ part9/patientor/backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patients from '../../data/patients';
-import { Patient, NonSensitivePatient } from '../types';
+import { Patient, NonSensitivePatient, NewPatient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getEntries = (): Patient[] => {
   return patients;
@@ -15,7 +16,17 @@ const getNonSensitiveEntries = (): NonSensitivePatient[] => {
   }));
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...patient,
+  };
+  patients.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getEntries,
   getNonSensitiveEntries,
+  addPatient,
 };
